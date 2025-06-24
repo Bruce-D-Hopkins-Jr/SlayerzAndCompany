81938f2a46afe8be2abaa3e062afc75e6ca1cc37
Bruce-D-Hopkins-Jr <brucedenhopk@outlook.com>
2025-06-23 23:57:59 -0400

    Finished primitive hero selection screen.

---
+++ Assets/Scripts/HeroDraftOptionUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HeroDraftOptionUI : MonoBehaviour
+{
+    public TextMeshProUGUI heroTypeText;
+    public Button selectButton;
+
+    private HeroCard heroData;
+    private HeroDraftManager heroDraftManager;
+
+    public void Setup(HeroCard hero, HeroDraftManager manager)
+    {
+        heroData = hero;
+        heroDraftManager = manager;
+        heroTypeText.text = hero.heroType.ToString();
+
+        selectButton.onClick.AddListener(() =>
+        {
+            heroDraftManager.SelectHero(heroData);
+            selectButton.interactable = false;
+        });
+    }
+}
