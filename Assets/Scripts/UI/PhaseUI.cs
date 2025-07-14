b134c813d1530df57e97be9de9fd3a0927c10bd4
Bruce-D-Hopkins-Jr <brucedenhopk@outlook.com>
2025-07-14 07:47:15 -0400

    New build.

---
+++ Assets/Scripts/UI/PhaseUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PhaseUI : MonoBehaviour
+{
+    [SerializeField] private Button endTurnButton;
+    [SerializeField] private TextMeshProUGUI phaseLabel;
+
+    private void Awake()
+    {
+        endTurnButton.onClick.AddListener(OnEndTurnClicked);
+        endTurnButton.gameObject.SetActive(false);
+    }
+
+    public void UpdateUI(GamePhase phase)
+    {
+        phaseLabel.text = $"Phase: {phase}";
+
+        // Only show button during SLAY phase
+        endTurnButton.gameObject.SetActive(phase == GamePhase.SLAY);
+    }
+
+    private void OnEndTurnClicked()
+    {
+        PhaseManager.Instance.SetCurrentPhase(GamePhase.MONSTER);
+        PhaseManager.Instance.AdvancePhase(); // SLAY → MONSTER
+    }
+}
