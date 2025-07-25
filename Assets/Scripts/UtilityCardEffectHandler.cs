c561c468c759e4fd045d1d7eebecd0d506754fef
Bruce-D-Hopkins-Jr <brucedenhopk@outlook.com>
2025-07-25 10:54:17 -0400

    Implement card effects.

---
+++ Assets/Scripts/UtilityCardEffectHandler.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public static class UtilityCardEffectHandler
+{
+    public static void ApplyEffect(UtilityCard card, MonoBehaviour target = null)
+    {
+        switch (card.EffectType)
+        {
+            case UtilityEffectType.HealSingle:
+                if (target.GetComponent<HeroCombatController>() != null)
+                {
+                    HeroCombatController hero = target.GetComponent<HeroCombatController>();
+                    hero.Heal(card.EffectValue);
+                }                    
+                break;
+
+            case UtilityEffectType.HealAll:
+                if (target.GetComponent<HeroCombatController>() != null)
+                {
+                    foreach (var h in GameObject.FindObjectsByType<HeroCombatController>(FindObjectsSortMode.None))
+                    {
+                        h.Heal(card.EffectValue);
+                    }                        
+                }                    
+                break;
+
+            case UtilityEffectType.DamageSingle:
+                if (target.GetComponent<MonsterCombatController>() != null)
+                {
+                    MonsterCombatController monster = target.GetComponent<MonsterCombatController>();
+                    monster.TakeDamage(card.EffectValue);
+                }                    
+                break;
+
+            case UtilityEffectType.DamageAll:
+                if (target.GetComponent<MonsterCombatController>() != null)
+                {
+                    foreach (var m in EncounterManager.Instance.GetActiveEncounterMonsters())
+                    {
+                        m.TakeDamage(card.EffectValue);
+                    }                        
+                }                
+                break;
+
+            case UtilityEffectType.DrawCards:
+                HandManager.Instance.DrawCards(DeckManager.Instance.Draw(card.EffectValue));
+                break;
+        }
+
+        Debug.Log($"[UtilityEffectHandler] {card.CardName} effect applied: {card.EffectType} ({card.EffectValue})");
+    }
+
+}
